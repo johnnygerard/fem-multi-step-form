195203b1cff21bb692499f9fc14498d85f144bbe
Johnny Gérard <john@jgerard.dev>
2024-02-12 18:05:20 +0100

    feat: add SVG icons

--- src/app/data/plan.array.ts
+++ src/app/data/plan.array.ts
@@ -1,20 +1,26 @@
+import { AdvancedSvgComponent } from "../svg/advanced-svg.component";
+import { ArcadeSvgComponent } from "../svg/arcade-svg.component";
+import { ProSvgComponent } from "../svg/pro-svg.component";
 import { Plan } from "../types/plan.type";
 
 export const plans: Plan[] = [
   {
+    icon: ArcadeSvgComponent,
     name: 'Arcade',
     monthlyPrice: 9,
   },
   {
+    icon: AdvancedSvgComponent,
     name: 'Advanced',
     monthlyPrice: 12,
   },
   {
+    icon: ProSvgComponent,
     name: 'Pro',
     monthlyPrice: 15,
   },
-].map(({ name, monthlyPrice }) => ({
-  icon: name.toLowerCase() + '.svg',
+].map(({ icon, name, monthlyPrice }) => ({
+  icon,
   name,
   monthlyPrice,
   yearlyPrice: monthlyPrice * 10, // 2 months free
--- src/app/types/plan.type.ts
+++ src/app/types/plan.type.ts
@@ -1,6 +1,15 @@
+import { AdvancedSvgComponent } from "../svg/advanced-svg.component";
+import { ArcadeSvgComponent } from "../svg/arcade-svg.component";
+import { ProSvgComponent } from "../svg/pro-svg.component";
+
 export type Plan = {
-  readonly icon: string;
   readonly name: string;
   readonly monthlyPrice: number;
   readonly yearlyPrice: number;
+  readonly icon: Icon;
 };
+
+type Icon =
+  | typeof ArcadeSvgComponent
+  | typeof AdvancedSvgComponent
+  | typeof ProSvgComponent;

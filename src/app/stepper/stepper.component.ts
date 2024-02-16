8aebc3123c4b20d0079aab8940da328bc969a142
Johnny Gérard <john@jgerard.dev>
2024-02-16 13:36:45 +0100

    feat: add confirmation component

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CdkStepper, CdkStepperModule } from '@angular/cdk/stepper';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { SubscriptionService } from '../subscription.service';
+import { ConfirmationComponent } from '../confirmation/confirmation.component';
 
 @Component({
   selector: 'app-stepper',
@@ -9,6 +10,7 @@ import { SubscriptionService } from '../subscription.service';
   imports: [
     CommonModule,
     CdkStepperModule,
+    ConfirmationComponent,
     AsyncPipe,
   ],
   providers: [
